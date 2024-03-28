d7ac0caba3cdc3556230bdeb9182040aba527069
Breno Xavier <brenoxs140@gmail.com>
2024-03-28 15:58:44 -0300

    trabalho todolist

--- todolist.js
+++ todolist.js
@@ -7,12 +7,15 @@ function add() {
     const newTask = document.createElement("div");
     const newH2 = document.createElement("h2");
     const texto = document.getElementById("task").value;
-    newH2.innerHTML = texto;
+    newH2.style.position = "relative";
+    newH2.style.top = "30px";
+
+    if (texto == "") {
+        alert("PREENCHE ESSA PORRA! :)");
+    } else {
+        newH2.innerHTML = texto;
     newTask.appendChild(newH2, );
     document.body.appendChild(newTask);
-
-    
-
 
     const btRemove = document.createElement("button");
     btRemove.innerHTML = "Remove";
@@ -23,15 +26,28 @@ function add() {
     const inputCheck = document.createElement("input");
     inputCheck.innerHTML = "Check";
     inputCheck.type = "checkbox";
+    inputCheck.style.width = "20px";
+    inputCheck.style.height = "20px";
+    inputCheck.style.color = "green";
+    inputCheck.style.position = "relative";
+    inputCheck.style.right = "273px";
+    inputCheck.style.bottom = "25px";
+
     newTask.appendChild(inputCheck);
     inputCheck.addEventListener("click", function(){  
         if(inputCheck.checked==true){
             newH2.style.textDecorationLine = "line-through";
-           
          } else {
             newH2.style.textDecorationLine = "none";
-         }
+         }});
 
-       })
+
+
+    }
+
+    
+
+
+    
     
     }
